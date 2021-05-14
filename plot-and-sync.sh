1d7a0b45af2322895cd118e036b23c27212b393e
Jason Ernst <ernstjason1@gmail.com>
2021-05-13 23:59:33 -0700

    trying to get key accept without yes/no

--- plot-and-sync.sh
+++ plot-and-sync.sh
@@ -3,7 +3,7 @@ ls -la ~/.ssh/
 cat ~/.ssh/config
 ssh-keygen -R $SYNC_HOST
 ssh-keyscan -t rsa $SYNC_HOST 2>&1 >> ~/.ssh/known_hosts
-ssh -vvv -o StrictHostKeyChecking=no $SYNC_HOST ls $SYNC_PATH || { echo "ssh test failed to $SYNC_HOST:$SYNC_PATH" ; exit 1; }
+ssh -o StrictHostKeyChecking=accept-new $SYNC_HOST ls $SYNC_PATH || { echo "ssh test failed to $SYNC_HOST:$SYNC_PATH" ; exit 1; }
 while true; do
   chia plots create -t /tmp -d /plots -r 4 -e -f $FARMER_KEY -p $POOL_KEY
   scp /plots/* $SYNC_HOST:$SYNC_PATH
